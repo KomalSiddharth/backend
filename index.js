45df79e63bd4b8a7f685b0ab43b890f0cd7e9115
KomalSiddharth <121292651+KomalSiddharth@users.noreply.github.com>
2024-09-09 19:08:44 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+console.log("radhey radhey");
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/home',(req,res)=>{
+    res.send('komal siddharth')
+})
+app.get('/login',(req,res)=>{
+    res.send('<h1>please login here </h1>')
+})
+
+app.listen(process.env.port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
